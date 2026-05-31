509573731965c8df3a9230469f77fc6cd5856e9c
ranajparida89 <ranajparida89@gmail.com>
2026-05-31 11:33:43 +0530

    new module for achivement added updated

--- routes/playerAchievementRoutes.js
+++ routes/playerAchievementRoutes.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = require("../db");
 
 /* =====================================================
    HEALTH CHECK
